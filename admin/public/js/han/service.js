7f9d665f8fa7573ef8ca45ef698b5cdb607d00ad
Nguyen Han <hanhan160598@gmail.com>
2021-05-31 15:25:02 +0700

    done module

--- admin/public/js/han/service.js
+++ admin/public/js/han/service.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    list_product_module();
+    list_service_module();
 });
 // module_about_us
 var id_module_service;
@@ -12,136 +12,69 @@ function list_service_module() {
         dataType: 'json',
         //headers: headers,
         success: function(response) {
-
+          
             if (response.success == 'true') {
+                $('#section1_service_title_module').val(response.data[0].module_title);
+           
+                id_module_service = response.data[0].id;
 
-                if (response.data[0].module_img != '') {
-                    $("#img_product_module_detail").attr("src", urlserver + response.data[0].module_img);
-                }
-
-                $('#section1_product_title_module').val(response.data[0].module_title);
-                let output = `
-                <button onclick="update_product_module(${response.data[0].id})" class="btn btn-green d-inline-block ml-3">Cập nhật</button>
-                `;
-                $('#section1_moudule_title').html(output);
-
-                ////section 2
-
-                $('#section2_product_title_module').val(response.data[1].module_title);
-                id_module_service = response.data[1].id;
-                list_product_product();
+               list_service_service();
             }
         }
     });
 }
 
-function update_product_module(id) {
-    try {
-        var fileToUpload = $('#img_product_module').prop('files')[0];
-
-        if (fileToUpload == undefined) {
-            fileToUpload = '123';
-        }
-        console.log(fileToUpload)
-        console.log($('#id_account').val());
-        console.log(id)
-        var formData = new FormData();
-        formData.append("detect", "product_module_manager");
-        formData.append("type_manager", "update_product_module");
-        formData.append("module_img", fileToUpload);
-        formData.append("id_module", id);
-        formData.append("id_account", $('#id_account').val());
-        $.ajax({
-            url: urlapi,
-            method: 'POST',
-            data: formData,
-            dataType: 'JSON',
-            contentType: false,
-            processData: false,
-            //headers: headers,
-            success: function(response) {
-
-                if (response.success == 'true') {
-                    alert(response.message);
-
-                } else {
-                    alert(response.message);
-                }
-            }
-        });
-    } catch (e) {
-        e.message;
-        console.log(e.message)
-    }
-
-}
-// list_product_product
-function list_product_product() {
+// list_service_service
+function list_service_service() {
     $.ajax({
         url: urlapi,
         method: 'POST',
         data: {
-            detect: 'product_product_manager',
-            type_manager: 'list_product_product',
+            detect: 'service_service_manager',
+            type_manager: 'list_service_service',
             id_account: $('#id_account').val(),
-            id_module_product: id_module_product
+            id_module_service: id_module_service
         },
         dataType: 'json',
         //headers: headers,
         success: function(response) {
+    
             var output = ``;
             if (response.success == 'true') {
                 response.data.forEach(function(item) {
                     output += `
                     <div class="wp-module">
-                    <div class="form-edit px-0 py-4">
-                        <div class="form-edits-row-check">
-                            <input type="checkbox" name="prod_hot" id="prod_hot" class="form-input-check">
-                            <label for="prod_hot" class="form-lable-check">Sản phẩm nổi bật</label>
-                        </div>
-                    </div>
-                    <div class="module w-100">
-                        <div class="module-title">
-                            <div class="heading">Module 1</div>
-                            <span data-tag="a" type="delete_module" onclick="show_delete_product_product(${item.id})" class="get_modal icon-close"><img src="public/images/x-black.png" alt=""></span>
-                        </div>
-                        <div class="form-edit p-0">
-                            <div class="form-edit-row my-3">
-                                <label class="form-lable">Tên sản phẩm</label>
-                                <input type="text" class="form-input" name="title" data-id="${item.id}" value="${item.product_title}" readonly>
+                        <div class="module">
+                            <div class="module-title">
+                                <div class="heading">Module 1</div>
+                                <span data-tag="a" onclick="show_delete_product_product(${item.id})" type="delete_module" class="get_modal icon-close"><img src="public/images/x-black.png" alt=""></span>
                             </div>
-                            <div class="form-edit-row my-3">
-                                <label class="form-lable">Danh mục</label>
-                                <input type="text" class="form-input" name="title" data-id="${item.id}" value="${item.product_category}" readonly>
+                            <div class="module-center  p-3">
+                                <span class="item"><img src="public/images/icon-badge-check.png" alt=""></span>
                             </div>
-                            <div class="form-file" data-target="process_2" data-id="1">
-                                <div class="form-file-box">
-                                    <span data-target="process_2" data-id="${item.id}" class="form-file-view-img d-block">
-                                        <img src="${urlserver+item.product_img}" alt="">
-                                    </span>
+                            <div class="form-edit p-0">
+                                <div class="form-edit-row my-3">
+                                    <label class="form-lable">Tiêu đề</label>
+                                    <input type="text" class="form-input" name="title" data-id="title_1" value="${item.service_title}" readonly>
+                                </div>
+                                <div class="form-edit-row my-3">
+                                    <label class="form-lable">Content</label>
+                                    <textarea readonly type="text" class="form-input" cols="30" rows="10" name="content" data-id="content_1">${item.service_content}</textarea>
                                 </div>
                             </div>
-                        </div>
-                        <div class="t-right">
-                            <span data-tag="a" onclick="show_update_product_product(${item.id})" type="edit_module" class="get_modal btn btn-green"> Cập nhật</span>
+                            <div class="t-right">
+                                <span data-tag="a" onclick="show_update_service_service(${item.id})" type="edit_module" class="get_modal btn btn-green">Cập nhật</span>
+                            </div>
                         </div>
                     </div>
-                </div>
                 `;
                 });
             }
             output += `
             <div class="wp-module">
-                <div class="form-edit px-0 py-4 visible-hidden">
-                    <div class="form-edits-row-check">
-                        <input type="checkbox" name="prod_hot" id="prod_hot" class="form-input-check">
-                        <label for="prod_hot" class="form-lable-check">Sản phẩm nổi bật</label>
-                    </div>
-                </div>
-                <span data-tag="a" onclick="show_create_product_product()" type="add_module" class="get_modal module-empty">
-                </span>
+                <span data-tag="a" onclick="show_create_service_service()" type="add_module" class="get_modal module-empty"></span>
             </div>`;
-            $('#list_product_product').html(output);
+            $('#list_service_service').html(output);
 
             var module_height = $('.module-empty').parent('.wp-module').prev().children('.module').outerHeight();
             $('.module-empty').css('height', module_height);
@@ -149,19 +82,19 @@ function list_product_product() {
     });
 }
 
-function show_create_product_product() {
-    $('#add_product_product').show();
+function show_create_service_service() {
+    $('#add_service_service').show();
 }
 
-function create_product_product() {
-    var fileToUpload = $('#img_product_product').prop('files')[0];
+function create_service_service() {
+    var fileToUpload = $('#img_service_service').prop('files')[0];
     var formData = new FormData();
-    formData.append("detect", "product_product_manager");
-    formData.append("type_manager", "create_product_product");
-    formData.append("product_title", $('#product_title').val());
-    formData.append("product_category", $('#product_category').val());
-    formData.append("id_module_product", id_module_product);
-    formData.append("product_img", fileToUpload);
+    formData.append("detect", "service_service_manager");
+    formData.append("type_manager", "create_service_service");
+    formData.append("service_title", $('#service_title').val());
+    formData.append("service_content", $('#service_content').val());
+    formData.append("id_module_service", id_module_service);
+    formData.append("service_img", fileToUpload);
     $.ajax({
         url: urlapi,
         method: 'POST',
@@ -173,10 +106,10 @@ function create_product_product() {
         success: function(response) {
             if (response.success == 'true') {
                 alert(response.message);
-                list_product_product();
-                $('#product_title').val('');
-                $('#product_category').val('');
-                $('#add_product_product').hide();
+                list_service_service();
+                $('#service_title').val('');
+                $('#service_content').val('');
+                $('#add_service_service').hide();
 
             } else {
                 alert(response.message);
@@ -185,48 +118,50 @@ function create_product_product() {
     });
 }
 
-function show_update_product_product(id) {
+function show_update_service_service(id) {
     $.ajax({
         url: urlapi,
         method: 'POST',
         data: {
-            detect: 'product_product_manager',
-            type_manager: 'list_product_product',
-            id_product: id,
+            detect: 'service_service_manager',
+            type_manager: 'list_service_service',
+            id_service: id,
         },
         dataType: 'json',
         //headers: headers,
         success: function(response) {
-            $('#title_product_product_detail').val(response.data[0].product_title);
-            $('#category_product_product_detail').val(response.data[0].product_category);
-            $("#img_product_product_detail1").attr("src", urlserver + response.data[0].product_img);
+         
+            $('#title_service_service_detail').val(response.data[0].service_title);
+            $('#content_service_service_detail').val(response.data[0].service_content);
+            $("#img_service_detail1").attr("src", urlserver + response.data[0].service_img);
             let output = `
-            <button id="add_file" onclick="update_product_product(${response.data[0].id})" class="btn-submit w-30 d-inline-block fz-1rem">Hoàn thành</button>
+            <button id="add_file" onclick="update_service_service(${response.data[0].id})" class="btn-submit w-30 d-inline-block fz-1rem">Hoàn thành</button>
             `;
-            $('#update_product_product').html(output);
+            $('#update_service_service').html(output);
         }
     });
 
-    $('#edit_product_product').show();
+    $('#edit_service_service').show();
 }
 
 $(".modal span.icon").click(function() {
     $(this).parent('.modal-title').parent('.modal-box').parent('.modal').fadeToggle();
 });
 
-function update_product_product(id) {
-    var fileToUpload = $('#img_product_detail').prop('files')[0];
+function update_service_service(id) {
+    console.log(id)
+    var fileToUpload = $('#img_service_detail').prop('files')[0];
     if (fileToUpload == undefined) {
         fileToUpload == '';
     }
-    console.log(fileToUpload)
+
     var formData = new FormData();
-    formData.append("detect", "product_product_manager");
-    formData.append("type_manager", "update_product_product");
-    formData.append("product_title", $('#title_product_product_detail').val());
-    formData.append("product_category", $('#category_product_product_detail').val());
-    formData.append("product_img", fileToUpload);
-    formData.append("id_product", id);
+    formData.append("detect", "service_service_manager");
+    formData.append("type_manager", "update_service_service");
+    formData.append("service_title", $('#title_service_service_detail').val());
+    formData.append("service_content", $('#content_service_service_detail').val());
+    formData.append("service_img", fileToUpload);
+    formData.append("id_service", id);
     $.ajax({
         url: urlapi,
         method: 'POST',
@@ -238,8 +173,8 @@ function update_product_product(id) {
         success: function(response) {
             if (response.success == 'true') {
                 alert(response.message);
-                list_product_product();
-                $('#edit_product_product').hide();
+                list_service_service();
+                $('#edit_service_service').hide();
             } else {
                 alert(response.message);
             }
@@ -248,27 +183,27 @@ function update_product_product(id) {
 }
 
 function show_delete_product_product(id) {
-    let output = `<button id="add_file" onClick="delete_product_product(${id})" class="btn-submit w-20 d-inline-block fz-1rem">Hoàn thành</button>`;
-    $('#btn_delete_product').html(output);
-    $('#delete_product_product').show();
+    let output = `<button id="add_file" onClick="delete_service_service(${id})" class="btn-submit w-20 d-inline-block fz-1rem">Hoàn thành</button>`;
+    $('#btn_delete_service').html(output);
+    $('#delete_service_service').show();
 }
 
-function delete_product_product(id) {
+function delete_service_service(id) {
     $.ajax({
         url: urlapi,
         method: 'POST',
         data: {
-            detect: 'product_product_manager',
-            type_manager: 'delete_product_product',
-            id_product: id,
+            detect: 'service_service_manager',
+            type_manager: 'delete_service_service',
+            id_service: id,
         },
         dataType: 'json',
         //headers: headers,
-        success: function(response) {
+        success: function(response) {   
             if (response.success == 'true') {
                 alert(response.message);
-                list_product_product();
-                $('#delete_product_product').hide();
+                list_service_service();
+                $('#delete_service_service').hide();
             } else {
                 alert(response.message);
             }
